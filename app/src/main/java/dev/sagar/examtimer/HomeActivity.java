e9d3379a533b59e1e9128ecbd7b74cc8776051f2
sagarpawardev <S@rov@r231>
2021-10-17 01:32:27 +0530

    chore: code clean up

--- app/src/main/java/dev/sagar/examtimer/HomeActivity.java
+++ app/src/main/java/dev/sagar/examtimer/HomeActivity.java
@@ -2,7 +2,6 @@ package dev.sagar.examtimer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,18 +17,15 @@ public class HomeActivity extends AppCompatActivity {
         final EditText etQuestions = findViewById(R.id.et_questions);
         Button btnStart = findViewById(R.id.btn_start);
 
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String txt = etQuestions.getText().toString();
-                int val = Integer.parseInt(txt);
-                etQuestions.setText("");
-                Bundle basket = new Bundle();
-                basket.putInt("q_count", val);
-                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-                intent.putExtras(basket);
-                startActivity(intent);
-            }
+        btnStart.setOnClickListener(view -> {
+            String txt = etQuestions.getText().toString();
+            int val = Integer.parseInt(txt);
+            etQuestions.setText("");
+            Bundle basket = new Bundle();
+            basket.putInt("q_count", val);
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            intent.putExtras(basket);
+            startActivity(intent);
         });
 
     }
